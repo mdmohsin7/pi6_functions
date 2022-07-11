ba4b456b2cf551ea7963296e2f8a2f305759ed3f
mdmohsin7 <mdmohsin_1@hotmail.com>
2022-07-11 17:03:58 +0530

    chore: code maintenance

---
+++ api_utils/utils.go
@@ -0,0 +1,22 @@
+package api_utils
+
+type Balance struct {
+	Denom  string `json:"denom"`
+	Amount string `json:"amount"`
+}
+type BalanceFromChain struct {
+	Balances []Balance `json:"balances"`
+}
+
+type FirebaseConfig struct {
+	Type                    string `json:"type"`
+	ProjectID               string `json:"project_id"`
+	PrivateKeyID            string `json:"private_key_id"`
+	PrivateKey              string `json:"private_key"`
+	ClientEmail             string `json:"client_email"`
+	ClientID                string `json:"client_id"`
+	AuthUri                 string `json:"auth_uri"`
+	TokenUri                string `json:"token_uri"`
+	AuthProviderX509CertUrl string `json:"auth_provider_x509_cert_url"`
+	ClientX509CertUrl       string `json:"client_x509_cert_url"`
+}
